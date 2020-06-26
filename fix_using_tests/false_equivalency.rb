11262aa05435f8f8c3e77dfdbfe9e53a2a26daac
Cody C <codyicastro@gmail.com>
2020-06-26 20:28:15 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -13,9 +13,9 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 2
+  if num == 2
      "HAM HAM HAM IN MY TUMMY"
-  elsif num = 1
+  elsif num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
   end
 end
